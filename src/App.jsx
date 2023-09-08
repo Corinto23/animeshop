793dc00aa1652e49e76a5ff49c01c2fad58795a5
Corinto <salazar72390@gmail.com>
2023-09-08 16:08:35 +0800

    change home to /

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <CartProvider>
         <Navbar />
         <Routes>
-          <Route path="home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="home" element={<Home />} />
           <Route path="manga" element={<Manga />} />
           <Route path="anime" element={<Anime />} />
